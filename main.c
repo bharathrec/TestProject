98e3964f20eb646285ea9b3e1aae55df786c6edb
Bharath <bharathkumar.j@gmail.com>
2017-08-26 18:25:41 +0400

    commit

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 int main()
 {
+ int q; 
+ printf("\nBharath\nEnter the value :");
+ scanf("%d",&q); 
  printf("\nTest Program\n");
+ printf("The square value is %d",q*q); 
  return 0;
 }
